ef0fa41671bbb3f779c1eb13bf980ad99d37b8ad
JifengJiang <jjian062@uottawa.ca>
2016-07-08 19:36:31 -0400

    php login

---
+++ dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+if(!mysql_connect("localhost","root","123456"))
+{
+	die('oops connection problem ! ¡ª> '.mysql_error());
+}
+if(!mysql_select_db("foodapp"))
+{
+	die('oops database selection problem ! ¡ª> '.mysql_error());
+}
+?>
